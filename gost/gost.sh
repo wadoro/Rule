e64c442013a7765d685956aa5925a52e360469d9
wadoro <wadoro@msn.com>
2020-09-14 23:20:25 +0800

    Update gost.sh

--- gost/gost.sh
+++ gost/gost.sh
@@ -1,12 +1,199 @@
-#!/usr/bin/env bash
-PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
-export PATH
+#! /bin/bash
 
-sh_ver=1.0.0
+sh_ver=1.0.1
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
+
+outinstalltls(){
+echo -n "是否自定义ssl证书y/n："
+read iftls
+if test $iftls = y;then
+echo "请输入tls证书地址格式为?cert=/path/to/my/cert/file&key=/path/to/my/key/file："
+read tlsaddress
+fi
+}
+
+installtls(){
+echo -n "请问是否验证证书y/n："
+read iftls
+if test $iftls = y;then
+tlsaddress="?secure=true"
+fi
+}
+
+check_outtls(){
+if test $protocol = "tls" -o $protocol = "https" -o $protocol = "wss" -o $protocol = "mwss" -o $protocol = "relay+tls";then
+if test $inorout -eq 1;then
+installtls
+else
+outinstalltls
+fi
+fi
+}
+
+installbefore(){
+echo -n "请问需要设置多少个入口？"
+read allinstallbeforenum
+installbeforenum=1
+while test "$installbeforenum" -le "$allinstallbeforenum"
+do
+echo -n "入口端口："
+read inport
+echo -n "目的地ip："
+read ipother
+echo -n "目的地端口："
+read portother
+if test "$installbeforenum" -eq "$allinstallbeforenum";then
+if test $protocol = "relay+tls";then
+echo "\""udp"://0.0.0.0:"$inport"/"$ipother":"$portother"\"," >> /etc/gost/config.json
+fi
+echo "\""tcp"://0.0.0.0:"$inport"/"$ipother":"$portother"\"" >> /etc/gost/config.json
+else
+if test $protocol = "relay+tls";then
+echo "\""udp"://0.0.0.0:"$inport"/"$ipother":"$portother"\"," >> /etc/gost/config.json
+fi
+echo "\""tcp"://0.0.0.0:"$inport"/"$ipother":"$portother"\"," >> /etc/gost/config.json
+fi
+installbeforenum=$((++installbeforenum))
+done
+}
+
+installafter(){
+echo -n "隧道出口ip："
+read outip
+echo -n "隧道出口端口："
+read outport
+echo -n "隧道出口账户："
+read outaccount
+echo -n "隧道密码："
+read outpassword
+}
+
+outinstallafter(){
+outip="0.0.0.0"
+echo -n "隧道出口端口："
+read outport
+echo -n "隧道出口账户："
+read outaccount
+echo -n "隧道密码："
+read outpassword
+}
+
+installnom(){
+echo "\""$protocol"://"$outaccount":"$outpassword"@"$outip":"$outport""$tlsaddress"\"" >> /etc/gost/config.json
+}
+
+confstart(){
+echo "{
+    \"Debug\": true,
+    \"Retries\": 0,
+    \"ServeNodes\": [" >> /etc/gost/config.json
+}
+
+confhalf(){
+echo "    ],
+    \"ChainNodes\": [" >> /etc/gost/config.json
+}
+
+conflast(){
+echo "    ]
+}" >> /etc/gost/config.json
+}
+
+check_inorout(){
+echo -n "请问是国内还是国外[1/2]："
+read inorout
+if test $inorout -eq 1;then
+ininstall
+else
+outinstall
+fi
+}
+
+check_protocol(){
+echo "请问是哪种安装协议 
+1 ws    2 tls    3 https    4 http 
+5 kcp   6 h2     7 h2c      8 quic 
+9 mws   10 wss   11 mwss    12 relay+tls"
+echo "----------"
+read numprotocol
+case "$numprotocol" in
+1)
+protocol=ws
+;;
+2)
+protocol=tls
+;;
+3)
+protocol=https
+;;
+4)
+protocol=http
+;;
+5)
+protocol=kcp
+;;
+6)
+protocol=h2
+;;
+7)
+protocol=h2c
+;;
+8)
+protocol=quic
+;;
+9)
+protocol=mws
+;;
+10)
+protocol=wss
+;;
+11)
+protocol=mwss
+;;
+12)
+protocol=relay+tls
+;;
+*)
+echo "$protocol is error"
+esac
+}
+
+outinstall(){
+confstart
+check_protocol
+outinstallafter
+check_outtls
+installnom
+conflast
+}
+
+ininstall(){
+confstart
+check_protocol
+installbefore
+confhalf
+check_outtls
+installafter
+installnom
+conflast
+}
+
+
+checknew(){
+checknew=$(gost -V 2>&1|awk '{print $2}')
+check_new_ver
+echo "你的gost版本为:"$checknew""
+echo -n 是否更新\(y/n\)\:
+read checknewnum
+if test $checknewnum = "y";then
+Install_ct
+else
+exit 0
+fi
+}
 
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
@@ -82,6 +269,7 @@ Install_ct(){
            check_file
            check_sys
            check_new_ver
+           `rm -rf gost-linux-"$bit"-"$ct_new_ver".gz`
            `wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
             `gunzip gost-linux-"$bit"-"$ct_new_ver".gz`
             `mv gost-linux-"$bit"-"$ct_new_ver" gost`
@@ -92,13 +280,9 @@ Install_ct(){
             `systemctl enable gost && systemctl restart gost`
             echo "------------------------------"
             if test -a /usr/bin/gost -a /usr/lib/systemctl/gost.service -a /etc/gost/config.json;then
-             echo "${Green_font_prefix}gost似乎安装成功${Font_color_suffix}"
-             `rm -rf "$(pwd)"/gost`
-             `rm -rf "$(pwd)"/gost.service`
-             `rm -rf "$(pwd)"/config.json`
-             `rm -rf "$(pwd)"/gost.sh`
+             echo "gost似乎安装成功"
             else
-            echo "${Red_font_prefix}gost没有安装成功，可以到https://www.fiisi.com评论留言询问${Font_color_suffix}"
+            echo "gost没有安装成功，可以到https://www.fiisi.com评论留言询问"
              `rm -rf   "$(pwd)"/gost`
              `rm -rf "$(pwd)"/gost.service`
              `rm -rf "$(pwd)"/config.json`
@@ -111,53 +295,80 @@ Uninstall_ct(){
              `rm -rf /usr/lib/systemd/system/gost.service`
              `rm -rf /etc/gost`
              `rm -rf "$(pwd)"/gost.sh`
-             echo "${Red_font_prefix}gost已经成功删除${Font_color_suffix}"
+             echo "gost已经成功删除"
 }
 
 Start_ct(){
           `systemctl start gost`
-          echo "${Green_font_prefix}已启动${Font_color_suffix}"
+          echo "已启动"
 }
 
 Stop_ct(){
           `systemctl stop gost`
-            echo "${Green_font_prefix}已停止${Font_color_suffix}"  
+            echo "已停止"  
 }
 
 Restart_ct(){
             `systemctl restart gost`
-             echo "${Green_font_prefix}已重启${Font_color_suffix}" 
+             echo "已重启" 
 }
-
-echo "the last version has been launched in my blog" 
-echo && echo -e "  gost 一键安装脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
+    
+echo && echo -e "  gost 一键安装脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix} 更新日期2020/3/13
   ---- fiisi | https://www.fiisi.com ----
   
  ${Green_font_prefix}1.${Font_color_suffix} 安装 gost
- ${Green_font_prefix}2.${Font_color_suffix} 卸载 gost
+ ${Green_font_prefix}2.${Font_color_suffix} 更新 gost
+ ${Green_font_prefix}3.${Font_color_suffix} 卸载 gost
 ————————————
- ${Green_font_prefix}3.${Font_color_suffix} 启动 gost
- ${Green_font_prefix}4.${Font_color_suffix} 停止 gost
- ${Green_font_prefix}5.${Font_color_suffix} 重启 gost
+ ${Green_font_prefix}4.${Font_color_suffix} 启动 gost
+ ${Green_font_prefix}5.${Font_color_suffix} 停止 gost
+ ${Green_font_prefix}6.${Font_color_suffix} 重启 gost
+————————————
+ ${Green_font_prefix}7.${Font_color_suffix} 配置gost
+ ${Green_font_prefix}8.${Font_color_suffix} 重新配置gost
+ ${Green_font_prefix}9.${Font_color_suffix} 输出gost配置
 ————————————" && echo
-read -e -p " 请输入数字 [1-5]:" num
+read -e -p " 请输入数字 [1-9]:" num
 case "$num" in
 	1)
 	Install_ct
 	;;
-	2)
+        2)
+        checknew
+        ;;
+	3)
 	Uninstall_ct
 	;;
-	3)
+	4)
 	Start_ct
 	;;
-	4)
+	5)
 	Stop_ct
 	;;
-	5)
+	6)
 	Restart_ct
 	;;
+        7)
+	rm -rf /etc/gost/config.json
+        check_inorout
+        `systemctl restart gost`
+	echo "your json"
+	echo "----------"
+	cat /etc/gost/config.json
+        ;;
+        8)
+        rm -rf /etc/gost/config.json
+	check_inorout
+        `chmod -R 777 /etc/gost/config.json`
+        `systemctl restart gost`
+	echo "your json"
+	echo "----------"
+	cat /etc/gost/config.json
+        ;;
+        9)
+        cat /etc/gost/config.json
+        ;;
 	*)
-	echo "请输入正确数字 [1-5]"
+	echo "请输入正确数字 [1-9]"
 	;;
 esac
