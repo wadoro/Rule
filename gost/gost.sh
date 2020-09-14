fa2e1e90bd7e4cc22a892606b52922ef2b2771ce
wadoro <wadoro@msn.com>
2020-09-14 23:15:26 +0800

    Update gost.sh

--- gost/gost.sh
+++ gost/gost.sh
@@ -87,8 +87,8 @@ Install_ct(){
             `mv gost-linux-"$bit"-"$ct_new_ver" gost`
             `mv gost /usr/bin/gost`
             `chmod -R 777 /usr/bin/gost`
-            `wget --no-check-certificate https://www.fiisi.com/gost/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
-            `mkdir /etc/gost && wget --no-check-certificate https://www.fiisi.com/gost/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost`
+            `wget --no-check-certificate https://raw.githubusercontent.com/wadoro/Rule/master/gost/gost.service && chmod -R 777 gost.service && mv gost.service /usr/lib/systemd/system`
+            `mkdir /etc/gost && wget --no-check-certificate https://raw.githubusercontent.com/wadoro/Rule/master/gost/config.json && mv config.json /etc/gost && chmod -R 777 /etc/gost`
             `systemctl enable gost && systemctl restart gost`
             echo "------------------------------"
             if test -a /usr/bin/gost -a /usr/lib/systemctl/gost.service -a /etc/gost/config.json;then
