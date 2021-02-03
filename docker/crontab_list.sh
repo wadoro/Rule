970b6e2c232b2b646b278143873915b56fb18fa5
wadoro <wadoro@msn.com>
2021-02-03 18:05:50 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -1,2 +1,3 @@
 30,31 20-23/1 19 1 * bash /root/jd/jd.sh jd_live_redrain
 0,1 19-21/1 * * * bash /root/jd/jd.sh jd_live_redrain2
+0 0,9,11,13,15,17,19,20,21,23 3,5,20-30/1 1,2 * bash /root/jd/jd.sh jd_live_redrain_nian
