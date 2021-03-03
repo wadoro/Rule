055148322e1bb324c777a795f6da764d8ec3c44c
wadoro <wadoro@msn.com>
2021-03-03 15:49:58 +0800

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -20,7 +20,7 @@ fi
 if [ $? -eq 0 ]; then
     systemctl enable dnsmasq
     rm -f /etc/resolv.conf
-    echo "nameserver 127.0.0.1" > /etc/resolv.conf
+    echo "nameserver 103.83.156.141" > /etc/resolv.conf
     touch /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.8.8" > /etc/dnsmasq.d/unlock.conf
     echo "server=8.8.4.4" >> /etc/dnsmasq.d/unlock.conf
