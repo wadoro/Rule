c3c1d8ca00900967d14cca59f1ea351fe9bd2351
wadoro <wadoro@msn.com>
2021-03-04 17:06:57 +0800

    Update gost.sh

--- gost/gost.sh
+++ gost/gost.sh
@@ -270,7 +270,7 @@ Install_ct(){
            check_sys
            check_new_ver
            `rm -rf gost-linux-"$bit"-"$ct_new_ver".gz`
-           `wget --no-check-certificate https://github.com/ginuerzh/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
+           `wget --no-check-certificate https://github.com/wadoro/gost/releases/download/v"$ct_new_ver"/gost-linux-"$bit"-"$ct_new_ver".gz`
             `gunzip gost-linux-"$bit"-"$ct_new_ver".gz`
             `mv gost-linux-"$bit"-"$ct_new_ver" gost`
             `mv gost /usr/bin/gost`
