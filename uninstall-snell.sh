e654bb3ae799bb9ad0ddf2b0e5fc537363e3ae41
wadoro <wadoro@msn.com>
2021-12-25 13:18:14 +0800

    Update uninstall-snell.sh

--- uninstall-snell.sh
+++ uninstall-snell.sh
@@ -3,8 +3,10 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 systemctl stop snell
 systemctl disable snell
-rm -f /ect/systemd/snell.service
-rm -f /usr/bin/snell-server
-rm -f /etc/snell/snell-server.conf
-rmdir /etc/snell/
+rm -rf /usr/local/bin/snell-server
+rm -rf /etc/systemd/system/snell.service
+rm -rf /etc/snell/snell-server.conf
+rm -rf /etc/snell
+rm -rf /root/snell*
+systemctl daemon-reload
 echo -e "Snell \033[1;32m 卸载完成 \033[0m"
